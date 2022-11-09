7ba231e3754bf82298db62d03fa923ed02ba4757
Syiceblue <65769834+Syiceblue@users.noreply.github.com>
2022-11-09 13:42:04 +0800

    Add files via upload

---
+++ maxArrSum.cpp
@@ -0,0 +1,32 @@
+#include<stdio.h>    
+int main()
+{
+    int i, n,sum=0,max=0;
+
+    scanf("%d", &n);
+    int a[50];
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]); 
+    }
+        
+    for (i=0;i<n;i++)
+	{
+        if(sum <= 0) 
+        {
+            sum = a[i];
+        }   
+        else
+        {
+            sum = sum +a[i];
+        } 
+           
+        if(sum > max)
+        {
+            max = sum; 
+        } 
+              
+    }
+    printf("最大子段和为%d",max);
+	return 0;
+}
